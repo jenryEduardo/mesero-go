cd4f6264dc33ada21f95d03a2dda76877c504490
diedgo05 <233358@ids.upchiapas.edu.mx>
2025-03-31 00:34:14 -0600

    Main corregido

--- part-2/api-secundary/main.go
+++ part-2/api-secundary/main.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"log"
+	dependenciesCircuito "second/circuito/infraestructure/dependencies"
+	routesCircuito "second/circuito/infraestructure/routes"
+	dependenciesHistorial "second/historial-de-entregas/infraestructure/dependencies"
+	routesHistorial "second/historial-de-entregas/infraestructure/routes"
+	dependenciesStatus "second/robot-status/infraestructure/dependencies"
+	routesStatus "second/robot-status/infraestructure/routes"
+	dependenciesRobot "second/robot/infraestructure/dependencies"
+	routesRobot "second/robot/infraestructure/routes"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:    []string{"*"},
+		AllowCredentials: true,
+	}))
+
+	//CIRCUITO
+	dependenciesCircuito.Init()
+	routesCircuito.CircuitRoutes(router)
+
+	//HISTORIAL
+	dependenciesHistorial.Init()
+	routesHistorial.HistorialRoutes(router)
+
+	//ROBOT
+	dependenciesRobot.Init()
+	routesRobot.RobotRoutes(router)
+
+	//ROBOT-STATUS
+	dependenciesStatus.Init()
+	routesStatus.RSroutes(router)
+
+	port := ":8081"
+	log.Println("Servidor en el puerto:", port)
+	log.Fatal(router.Run(port))
+}
