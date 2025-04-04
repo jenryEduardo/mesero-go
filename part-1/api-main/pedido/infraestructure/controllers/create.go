09f6b9d13d884deeb4f9e897c38555f64e7c7a43
jenryEduardo <eduardogomezcalvo851@gmail.com>
2025-04-03 23:36:02 -0600

    finish FCMS

--- part-1/api-main/pedido/infraestructure/controllers/create.go
+++ part-1/api-main/pedido/infraestructure/controllers/create.go
@@ -4,6 +4,8 @@ import (
 	"api-main/pedido/application"
 	"api-main/pedido/domain"
 	"api-main/pedido/infraestructure"
+	"bytes"
+	"encoding/json"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -23,9 +25,24 @@ func CreatePedido(c *gin.Context){
 	 id,err:=useCase.Execute(pedido);
 	if err!=nil{
 		c.JSON(http.StatusBadRequest,gin.H{"error":err})
+		return
 	}
   
-	c.JSON(http.StatusOK,gin.H{"ok":id})
+	payload, err := json.Marshal(gin.H{"id": id})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al crear el JSON de salida"})
+		return
+	}
+
+	// Enviar el JSON al otro servidor
+	resp, err := http.Post("http://localhost:3002/consumer/", "application/json", bytes.NewBuffer(payload))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al enviar el ID al otro servidor"})
+		return
+	}
+	defer resp.Body.Close()
+
+	c.JSON(http.StatusOK, gin.H{"ok": id, "message": "ID enviado correctamente"})
 
 
 }
